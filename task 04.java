91d800a4cdea1227176a164dabd0c238a448631f
abdullah65325 <108948084+abdullah65325@users.noreply.github.com>
2025-01-19 18:32:44 +0500

    Lab task 04.java

---
+++ task 04.java
@@ -0,0 +1,10 @@
+
+
+public class MathExpression {
+    public static void main(String[] args) {
+        // Calculating the expression (10+5)*(4-6)/4
+        double result = (10 + 5) * (4 - 6) / 4.0;
+        
+        System.out.println("The result of the expression (10+5)*(4-6)/4 is: " + result);
+    }
+}
