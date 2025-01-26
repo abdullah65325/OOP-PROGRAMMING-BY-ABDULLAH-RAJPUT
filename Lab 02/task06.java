26ff33f751a54e511c53df674abd35c9ca4d5483
abdullah65325 <108948084+abdullah65325@users.noreply.github.com>
2025-01-26 19:11:05 +0500

    Add files via upload

---
+++ Lab 02/task06.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        String result = (age >= 18) ? "You are eligible for voting." : "You are not eligible for voting.";
+        
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
