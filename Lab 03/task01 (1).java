298682e7496c0a73856d12ed700645865c2b757c
abdullah65325 <108948084+abdullah65325@users.noreply.github.com>
2025-02-09 19:03:31 +0500

    Add files via upload

---
+++ Lab 03/task01 (1).java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the range from the user
+        System.out.print("Enter the starting number: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter the ending number: ");
+        int end = scanner.nextInt();
+
+        // Loop through all numbers in the range
+        for (int num = start; num <= end; num++) {
+            boolean isPrime = true;
+
+            // Check if num is divisible by any number other than 1 and itself
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            // If num is not divisible by any number, it's prime
+            if (isPrime && num > 1) {
+                System.out.println(num);
+            }
+        }
+
+        scanner.close();
+    }
+}
